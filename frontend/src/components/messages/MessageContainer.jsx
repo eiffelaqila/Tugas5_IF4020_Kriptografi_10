d1b667dc0b2a715ecc3bdb471107b40206b56a80
Alifia Rahmah <alifiarahmah03@gmail.com>
2024-05-21 11:20:25 +0700

    feat: add key input before conversation

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,35 +1,72 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from 'react';
+import { TiMessages } from "react-icons/ti";
+import { useAuthContext } from "../../context/AuthContext";
 import useConversation from "../../store/useConversation";
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
-import { TiMessages } from "react-icons/ti";
-import { useAuthContext } from "../../context/AuthContext";
 
 const MessageContainer = () => {
-    const { selectedConversation, setSelectedConversation } = useConversation();
+  const { selectedConversation, setSelectedConversation } = useConversation();
+  const [publicKey, setPublicKey] = useState('');
+  const [privateKey, setPrivateKey] = useState('');
+  const pubKeyRef = useRef();
+  const privKeyRef = useRef();
 
-    useEffect(() => {
+  useEffect(() => {
 		// cleanup function (unmounts)
 		return () => setSelectedConversation(null);
 	}, [setSelectedConversation]);
 
-	return (
-		<div className='sm:min-w-[300px] md:min-w-[450px] flex flex-col'>
-            {!selectedConversation ? (
-                <NoChatSelected />
-            ) : (
-                <>
-                    {/* Header */}
-                    <div className='bg-slate-500 px-4 py-2 mb-2'>
-                        <span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>{selectedConversation.username}</span>
-                    </div>
+  useEffect(() => {
+    setPublicKey('');
+    setPrivateKey('');
+  }, [selectedConversation]);
 
-                    <Messages />
-                    <MessageInput />
-                </>
-            )}
-		</div>
-	);
+	return (
+    <div className="sm:min-w-[300px] md:min-w-[450px] flex flex-col">
+      {!selectedConversation ? (
+        <NoChatSelected />
+      ) : !publicKey && !privateKey ? (
+        <div className="p-10 justify-center align-center flex flex-col gap-5">
+          <div>
+            <input
+              className='border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white'
+              placeholder="Your public key"
+              ref={pubKeyRef}
+            />
+          </div>
+          <div>
+            <input
+              className='border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white'
+              placeholder={`${selectedConversation.username}'s private key`}
+              ref={privKeyRef}
+            />
+          </div>
+          <button
+            onClick={() => {
+              setPublicKey(pubKeyRef.current.value);
+              setPrivateKey(privKeyRef.current.value);
+            }}
+          >
+            Enter chat
+          </button>
+        </div>
+      ) : (
+        <>
+          {/* Header */}
+          <div className="bg-slate-500 px-4 py-2 mb-2">
+            <span className="label-text">To:</span>{' '}
+            <span className="text-gray-900 font-bold">
+              {selectedConversation.username}
+            </span>
+          </div>
+
+          <Messages />
+          <MessageInput />
+        </>
+      )}
+    </div>
+  );
 };
 
 const NoChatSelected = () => {
