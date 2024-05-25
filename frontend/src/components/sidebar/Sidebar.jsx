37e55ab780cb721e62df3b3f2275ab0d1fa3c878
Alifia Rahmah <alifiarahmah03@gmail.com>
2024-05-25 18:38:30 +0700

    feat: add logged in username in sidebar

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { useAuthContext } from "../../context/AuthContext";
 import Conversations from "./Conversations";
 import E2eeButton from "./E2eeButton";
 import LogoutButton from "./LogoutButton";
@@ -5,6 +6,7 @@ import SchnorrButton from "./SchnorrButton";
 import SearchInput from "./SearchInput";
 
 const Sidebar = () => {
+  const { authUser } = useAuthContext();
 	return (
 		<div className='flex flex-col p-4 border-r border-slate-500'>
 			<SearchInput />
@@ -16,7 +18,10 @@ const Sidebar = () => {
         <SchnorrButton />
       </div>
 			<div className='px-3 divider'></div>
+      <div className="flex justify-between">
 			<LogoutButton />
+      <p>Logged in as <b>{authUser.username}</b></p>
+      </div>
 		</div>
 	);
 };
